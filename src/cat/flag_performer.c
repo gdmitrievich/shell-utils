14b53db22843ebe700ed59a93d39629a7b5058bb
Gleb Mikhailov <gmworklife25@gmail.com>
2024-12-09 16:18:56 +0300

    feat: Add process_b_flag_on_line func

--- src/cat/flag_performer.c
+++ src/cat/flag_performer.c
@@ -2,12 +2,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void process_flags(flags flags, int first_filepath_idx, int argc, char** argv) {
     char line[256] = {0};
     for (int i = first_filepath_idx; i < argc; ++i) {
         char* state = NULL;
-        FILE* f = fopen(argv[i], "r");
+        FILE* f = fopen(argv[i], "rb");
 
         while ((state = fgets(line, sizeof(line), f))) process_flags_on_line(flags, line);
 
@@ -17,9 +18,22 @@ void process_flags(flags flags, int first_filepath_idx, int argc, char** argv) {
 
 void process_flags_on_line(flags flags, const char* line) {
     if (flags.b) process_b_flag_on_line(line);
-    if (flags.E) process_E_flag_on_line(line);
-    if (flags.n) process_n_flag_on_line(line);
-    if (flags.s) process_s_flag_on_line(line);
-    if (flags.T) process_T_flag_on_line(line);
-    if (flags.v) process_v_flag_on_line(line);
+    //if (flags.E) process_E_flag_on_line(line);
+    //if (flags.n) process_n_flag_on_line(line);
+    //if (flags.s) process_s_flag_on_line(line);
+    //if (flags.T) process_T_flag_on_line(line);
+    //if (flags.v) process_v_flag_on_line(line);
+}
+
+void process_b_flag_on_line(const char* line) {
+	static int nLine = 1;
+    if (!is_fully_empty_line(line)) {
+		printf("%6d\t%s", nLine++, line);
+    } else {
+        printf("%s", line);
+    }
+}
+
+int is_fully_empty_line(const char* line) {
+	return line[0] == '\n';
 }
--- src/cat/flag_performer.h
+++ src/cat/flag_performer.h
@@ -5,5 +5,7 @@
 
 void process_flags(flags flags, int first_filepath_idx, int argc, char** argv);
 void process_flags_on_line(flags flags, const char* line);
+void process_b_flag_on_line(const char* line);
+int is_fully_empty_line(const char* line);
 
 #endif // FLAG_PERFORMER_H
