7a63e3d57e88aa028de30a20f1c2aea91e54edf9
Gleb Mikhailov <gmworklife25@gmail.com>
2024-12-09 18:14:12 +0300

    fix: Fix bug with reading two or more files
    Now it combines two lines both the last line of the first file and the
    first of the second - it works in case when the first file doesn't have
    new line char.

--- src/cat/flag_performer.c
+++ src/cat/flag_performer.c
@@ -5,35 +5,75 @@
 #include <string.h>
 
 void process_flags(flags flags, int first_filepath_idx, int argc, char** argv) {
-    char line[256] = {0};
-    for (int i = first_filepath_idx; i < argc; ++i) {
+    char line[512] = {0};
+	int i = first_filepath_idx;
+    while (i < argc) {
         char* state = NULL;
-        FILE* f = fopen(argv[i], "rb");
+        FILE* f = fopen(argv[i], "r");
+		if (!f) {
+			perror("cat");
+			exit(EXIT_FAILURE);
+		}
 
-        while ((state = fgets(line, sizeof(line), f))) process_flags_on_line(flags, line);
+        while ((state = fgets(line, sizeof(line), f))) {
+			if (!has_new_line_char_at_the_end(line) && fpeek(f) == EOF && i + 1 < argc) {
+				++i;
+				fclose(f);
+				f = read_new_line(line, &i, argc, argv);
+			}
+            process_flags_on_line(flags, line);
+        }
 
         fclose(f);
+		++i;
     }
+}
+
+int has_new_line_char_at_the_end(const char* line) {
+	return line[strlen(line) - 1] == '\n';
+}
+
+int fpeek(FILE* f) {
+	int c = fgetc(f);
+	return ungetc(c, f);
+}
+
+FILE* read_new_line(char* line, int* i, int argc, char** argv) {
+	FILE* f = fopen(argv[*i], "r");
+	if (f == NULL) {
+		perror("cat");
+		exit(EXIT_FAILURE);
+	}
+	char l[256] = {0};
+	if (fgets(l, sizeof(l), f)) {
+		strcat(line, l);
+
+		if (!has_new_line_char_at_the_end(line) && fpeek(f) == EOF && *i + 1 < argc) {
+			++*i;
+			fclose(f);
+			f = read_new_line(line, i, argc, argv);
+		}
+	}
+
+	return f;
 }
 
 void process_flags_on_line(flags flags, const char* line) {
     if (flags.b) process_b_flag_on_line(line);
-    //if (flags.E) process_E_flag_on_line(line);
-    //if (flags.n) process_n_flag_on_line(line);
-    //if (flags.s) process_s_flag_on_line(line);
-    //if (flags.T) process_T_flag_on_line(line);
-    //if (flags.v) process_v_flag_on_line(line);
+    // if (flags.E) process_E_flag_on_line(line);
+    // if (flags.n) process_n_flag_on_line(line);
+    // if (flags.s) process_s_flag_on_line(line);
+    // if (flags.T) process_T_flag_on_line(line);
+    // if (flags.v) process_v_flag_on_line(line);
 }
 
 void process_b_flag_on_line(const char* line) {
-	static int nLine = 1;
+    static int nLine = 1;
     if (!is_fully_empty_line(line)) {
-		printf("%6d\t%s", nLine++, line);
+        printf("%6d\t%s", nLine++, line);
     } else {
         printf("%s", line);
     }
 }
 
-int is_fully_empty_line(const char* line) {
-	return line[0] == '\n';
-}
+int is_fully_empty_line(const char* line) { return line[0] == '\n'; }
--- src/cat/flag_performer.h
+++ src/cat/flag_performer.h
@@ -2,8 +2,13 @@
 #define FLAG_PERFORMER_H
 
 #include "flags.h"
+#include <stdio.h>
 
 void process_flags(flags flags, int first_filepath_idx, int argc, char** argv);
+int has_new_line_char_at_the_end(const char* line);
+int fpeek(FILE* f);
+FILE* read_new_line(char* line, int* i, int argc, char** argv);
+
 void process_flags_on_line(flags flags, const char* line);
 void process_b_flag_on_line(const char* line);
 int is_fully_empty_line(const char* line);
