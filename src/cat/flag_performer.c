444c146422a9ab4309082cd440891886259c8f93
Gleb Mikhailov <gmworklife25@gmail.com>
2024-12-10 07:57:38 +0300

    style: Style changes

--- src/cat/flag_performer.c
+++ src/cat/flag_performer.c
@@ -11,7 +11,7 @@ void process_flags(flags flags, int first_filepath_idx, int argc, char** argv) {
     int i = first_filepath_idx;
     while (i < argc) {
         FILE* f = fopen(argv[i], "r");
-		print_error_if_cant_open_file("cat", argv[i], f);
+        print_error_if_cant_open_file("cat", argv[i], f);
 
         char* state = NULL;
         while ((state = fgets(line, sizeof(line), f))) {
@@ -43,7 +43,7 @@ int fpeek(FILE* f) {
 
 FILE* read_line_in_new_file(char* line, int* i, int argc, char** argv) {
     FILE* f = fopen(argv[*i], "r");
-	print_error_if_cant_open_file("cat", argv[*i], f);
+    print_error_if_cant_open_file("cat", argv[*i], f);
 
     char l[256] = {0};
     if (fgets(l, sizeof(l), f)) {
@@ -137,7 +137,7 @@ int is_tab(char ch) { return ch == '\t'; }
 void process_v_flag_on_line(char* line) {
     if (!line) return;
 
-	char* new_line = (char*) try_allocate_memory("cat", strlen(line) * 4 + 1);
+    char* new_line = (char*)try_allocate_memory("cat", strlen(line) * 4 + 1);
 
     size_t l = strlen(line);
     for (size_t i = 0; i < l; ++i) {
