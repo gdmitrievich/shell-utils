e4c867da76d30f96952cb604b7a2da9193d3dce9
Gleb Mikhailov <gmworklife25@gmail.com>
2024-12-11 07:46:37 +0300

    fix: Fix sizeof-pointer-memaccess error

--- src/grep/regex_matches.c
+++ src/grep/regex_matches.c
@@ -3,15 +3,17 @@
 #include <string.h>
 
 void init_regex_matches(regex_matches* rm) {
-	for (int i = 0; i < sizeof(rm->matched_files); ++i) {
-		for (int j = 0; j < sizeof(rm->matched_files[0].matched_lines); ++j) {
-			matched_line* m_l = &rm->matched_files[i].matched_lines[j];
-			char* l_content = m_l->content;
-			memset(l_content, 0, sizeof(l_content));
-			m_l->number = 0;
-		}
+    for (size_t i = 0; i < sizeof(rm->matched_files); ++i) {
+        for (size_t j = 0; j < sizeof(rm->matched_files[0].matched_lines); ++j) {
+            matched_line* m_l = &rm->matched_files[i].matched_lines[j];
+            char* l_content = m_l->content;
+            size_t s = sizeof(l_content);
+            memset(l_content, 0, s);
+            m_l->number = 0;
+        }
 
-		char* f_name = rm->matched_files[i].name;
-		memset(f_name, 0, sizeof(f_name));
-	}
+        char* f_name = rm->matched_files[i].name;
+        size_t s = sizeof(f_name);
+        memset(f_name, 0, s);
+    }
 }
--- src/grep/regex_matches.h
+++ src/grep/regex_matches.h
@@ -2,19 +2,19 @@
 #define REGEX_MATCHES
 
 typedef struct {
-	int number;
-	char content[1024];
+    int number;
+    char content[1024];
 } matched_line;
 
 typedef struct {
-	char name[1024];
-	matched_line matched_lines[1024];
+    char name[1024];
+    matched_line matched_lines[1024];
 } matched_file;
 
 typedef struct {
-	matched_file matched_files[1024];
+    matched_file matched_files[1024];
 } regex_matches;
 
 void init_regex_matches(regex_matches* rm);
 
-#endif // REGEX_MATCHES
+#endif  // REGEX_MATCHES
