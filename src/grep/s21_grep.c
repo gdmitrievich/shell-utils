c1a512756a4f3c134583e97663473e2a76d06bf5
Gleb Mikhailov <gmworklife25@gmail.com>
2024-12-16 18:13:47 +0300

    refactor: Move code written on main to s21_grep func

--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -6,42 +6,23 @@
 #include "matched_line.h"
 #include "output.h"
 #include "search.h"
+#include "s21_grep.h"
 
 int main(int argc, char** argv) {
+	s21_grep(argc, argv);
+	return 0;
+}
+
+void s21_grep(int argc, char** argv) {
     cmd_args_data cad;
     init_cad(&cad);
     bool status = set_retrieved_cmd_arg_data(&cad, argc, argv);
-    if (status) {
-        printf("flags: ");
-        if (cad.flags.e) printf("e");
-        if (cad.flags.i) printf("i");
-        if (cad.flags.v) printf("v");
-        if (cad.flags.c) printf("c");
-        if (cad.flags.l) printf("l");
-        if (cad.flags.n) printf("n");
-        if (cad.flags.h) printf("h");
-        if (cad.flags.s) printf("s");
-        if (cad.flags.f) printf("f");
-        if (cad.flags.o) printf("o");
-        printf("\npatterns: %s\n", cad.patterns);
-        printf("pattern_files: ");
-        for (size_t i = 0; cad.pattern_files && cad.pattern_files[i] != NULL; ++i) {
-            printf("%s, ", cad.pattern_files[i]);
-        }
-        printf("\nsearch_files: ");
-        for (size_t i = 0; cad.search_files && cad.search_files[i] != NULL; ++i) {
-            printf("%s, ", cad.search_files[i]);
-        }
-        printf("\nstatus: %d", status);
-    }
-    return 0;
-    // matched_line* m_lines = NULL;
-    // if (status) status = set_matched_lines(&m_lines, &cad);
-    // if (status) output(m_lines, &cad);
+    matched_line* m_lines = NULL;
+    if (status) status = set_matched_lines(&m_lines, &cad);
+    if (status) output(m_lines, &cad);
 
-    // free_matched_lines(m_lines);
-    // free_cad(cad);
+    free_matched_lines(m_lines);
+    free_cad(cad);
 
-    // if (!status && errno != 0) print_error("grep", NULL);
-    return 0;
+    if (!status && errno != 0) print_error("grep", NULL);
 }
---
+++ src/grep/s21_grep.h
@@ -0,0 +1,6 @@
+#ifndef S21_GREP_H
+#define S21_GREP_H
+
+void s21_grep(int argc, char** argv);
+
+#endif // S21_GREP_H
