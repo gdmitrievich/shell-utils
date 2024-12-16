15427d2072490abddf6c99703cf4de83d63d9c22
Gleb Mikhailov <gmworklife25@gmail.com>
2024-12-16 18:09:22 +0300

    refactor: Change the way of getting patterns from more than one file

--- src/grep/search.h
+++ src/grep/search.h
@@ -2,12 +2,16 @@
 #define SEARCH_H
 
 #include <regex.h>
+#include <stdio.h>
 
 #include "cmd_args_data.h"
 #include "matched_line.h"
 
 bool set_matched_lines(matched_line** m_lines_ptr, cmd_args_data* cad);
-bool set_retrieved_regexes_from_file(char** pattern_ptr, const char* pattern_file);
+bool set_regexes_retrieved_from_files(char** patterns_ptr, char** pattern_files);
+bool set_regexes_retrieved_from_file(char** patterns_ptr, FILE* fp);
+int has_new_line_char_at_the_end(const char* line);
+
 bool set_reg_exec_results_as_matched_lines(matched_line** m_lines_ptr, const cmd_args_data* cmd);
 bool set_all_matches_from_line(matched_line** m_lines, regex_t* regex, char* str, regmatch_t* rm,
                                const char* file_name, size_t line_number);
