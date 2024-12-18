8c293c434a847e837f0298a8c21ab717a5f67529
Gleb Mikhailov <gmworklife25@gmail.com>
2024-12-18 18:54:53 +0300

    refactor: Add some changes

--- src/grep/pattern_files_reader.c
+++ src/grep/pattern_files_reader.c
@@ -10,7 +10,7 @@ bool set_regexes_retrieved_from_files(char** patterns_ptr, char** pattern_files)
     for (size_t i = 0; status && pattern_files[i] != NULL; ++i) {
         FILE* fp = fopen(pattern_files[i], "r");
         if (fp) {
-            set_regexes_retrieved_from_file(patterns_ptr, fp);
+            status = set_regexes_retrieved_from_file(patterns_ptr, fp);
             fclose(fp);
         } else {
             print_error("grep", pattern_files[i]);
@@ -27,11 +27,11 @@ bool set_regexes_retrieved_from_file(char** patterns_ptr, FILE* fp) {
     while (status && fgetdyns(&line, &line_len, fp) != NULL) {
         if (has_new_line_char_at_the_end(line)) line[strlen(line) - 1] = '\0';
 
-        if (*line == '\0') {
-            if (!add_pattern_to_patterns_string(patterns_ptr, ".")) status = false;
-        } else {
-            if (!add_pattern_to_patterns_string(patterns_ptr, line)) status = false;
-        }
+        if (*line == '\0')
+            if (!add_pattern_to_patterns_string(patterns_ptr, "."))
+                status = false;
+            else if (!add_pattern_to_patterns_string(patterns_ptr, line))
+                status = false;
         free(line);
         line = NULL;
     }
